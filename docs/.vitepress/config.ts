56244e31670605e68d184626358bee305da510be
seokju-na <seokju.me@toss.im>
2025-01-30 02:16:43 +0900

    docs: remove sandpack, apply toss product sans font (#37)
    * remove sandpack
    
    * theme

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,4 @@
-import container from 'markdown-it-container';
 import { defineConfig } from 'vitepress';
-import { renderSandbox } from 'vitepress-plugin-sandpack';
 import { en } from './en.js';
 import { ko } from './ko';
 import { shared } from './shared.js';
@@ -10,14 +8,5 @@ export default defineConfig({
   locales: {
     root: { label: 'English', ...en },
     ko: { label: '한국어', ...ko },
-  },
-  markdown: {
-    config(md) {
-      md.use(container, 'sandpack', {
-        render(tokens: any[], idx: number) {
-          return renderSandbox(tokens, idx, 'sandpack');
-        },
-      });
-    },
   },
 });
